11e65f1b70483e1ef3e5cfd51c9d10c213d4fd81
JericFederer <pianocoderodyssey@gmail.com>
2024-11-12 14:30:22 +0900

    removed headers() in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -31,20 +31,20 @@ const nextConfig: NextConfig = {
 };
 
 module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       // matching all API routes
+  //       source: "/api/:path*",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  //         { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+  //       ]
+  //     }
+  //   ]
+  // },
   // ! Error: Invalid src prop (https://avatars.githubusercontent.com/u/88467002?v=4) on next/image, hostname "avatars.githubusercontent.com" is not configured under images in your next.config.js
   images: {
     domains: ['avatars.githubusercontent.com'],  // Add this domain
